bf6609053bd450edb5fa923118160c693fe3f09e
chewbacca <chewbacca88@gmx.de>
2012-10-07 19:03:20 +0100

    initialized DTOCard class

---
+++ src/dto/DTOCard.java
@@ -0,0 +1,13 @@
+package dto;
+
+/**
+ * User: Timm Herrmann
+ * Date: 07.10.12
+ * Time: 14:34
+ */
+public class DTOCard {
+  public short cardType;
+  public boolean movable;
+  public short cardValue;
+  public short cardColor;
+}
